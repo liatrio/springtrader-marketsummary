cbe654bb401df1cb9e159619e788356bc6d1ce1a
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-07-09 10:31:16 -0700

    generated spring initializer files

---
+++ src/main/java/org/springframework/nanotrader/springtradermarketSummary/SpringtraderMarketSummaryApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.nanotrader.springtradermarketSummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtraderMarketSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtraderMarketSummaryApplication.class, args);
+	}
+
+}
