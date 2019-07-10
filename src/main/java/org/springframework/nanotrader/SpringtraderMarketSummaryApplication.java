46c4c4aeaa5ed80bf29d39f6eaefc9fdcf215673
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-07-10 16:22:31 -0700

    added dependencies to get project to run

--- src/main/java/org/springframework/nanotrader/SpringtraderMarketSummaryApplication.java
+++ src/main/java/org/springframework/nanotrader/SpringtraderMarketSummaryApplication.java
@@ -1,10 +1,12 @@
-package org.springframework.nanotrader.springtradermarketSummary;
+package org.springframework.nanotrader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.nanotrader.web.controller.MarketSummaryController;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SpringtraderMarketSummaryApplication {
 
 	public static void main(String[] args) {
