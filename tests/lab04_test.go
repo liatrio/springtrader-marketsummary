eeac63d1c3b1a23e3a5db2f1de1e97aa27ef3f27
Takumi Nishida <takumin@liatrio.com>
2020-05-12 16:25:23 -0700

    Update test names (#95)
    * updating test names and package
    
    * remove comment

--- tests/lab04_test.go
+++ tests/lab04_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Lab 4 Deployments", func() {
+var _ = Describe("Lab 4", func() {
 	var failMessage string
 
 	BeforeEach(func() {
@@ -25,6 +25,27 @@ var _ = Describe("Lab 4 Deployments", func() {
 		It("should have a values.yaml file", func() {
 			failMessage = "values.yaml doesn't exist or is in the wrong location\n"
 			Expect("../charts/marketsummary/values.yaml").To(BeAnExistingFile(), failMessage)
+		})
+	})
+
+	Context("Step 3", func() {
+		It("should have a CurrencyUtils.java file", func() {
+			failMessage = "CurrencyUtils.java doesn't exist or is in the wrong location\n"
+			Expect("../src/main/java/org/springframework/nanotrader/data/util/CurrencyUtils.java").To(BeAnExistingFile(), failMessage)
+		})
+	})
+
+	Context("Step 4", func() {
+		It("should have a MarketSummaryController.java file", func() {
+			failMessage = "MarketSummaryController.java doesn't exist or is in the wrong location\n"
+			Expect("../src/main/java/org/springframework/nanotrader/web/controller/MarketSummaryController.java").To(BeAnExistingFile(), failMessage)
+		})
+	})
+
+	Context("Step 5", func() {
+		It("should have a build.gradle file", func() {
+			failMessage = "build.gradle doesn't exist or is in the wrong location\n"
+			Expect("../build.gradle").To(BeAnExistingFile(), failMessage)
 		})
 	})
 
