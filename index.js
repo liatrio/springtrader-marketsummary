fa61ad534e14da654ea30ca431c591f65c90bb5b
Michael Parker <michael@parker.gg>
2020-06-08 17:22:45 -0500

    add tracing

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const hapi = require("@hapi/hapi");
+const { initTracer, opentracing } = require("jaeger-client");
+const { promisify } = require("util");
 
 const repository = require("./src/repository");
 const { loadQuoteData } = require("./src/repository/data");
 const marketSummaryController = require("./src/controller/marketsummary");
+
+const tracer = initTracer(
+    {
+        serviceName: "marketSummary",
+        sampler: {
+            type: "const",
+            param: 1,
+        },
+        reporter: {
+            logSpans: true, // this logs whenever we send a span
+            collectorEndpoint: "http://localhost:14268/api/traces",
+        },
+    },
+    {
+        logger: {
+            info: function (msg) {
+                console.log("INFO  ", msg);
+            },
+            error: function (msg) {
+                console.log("ERROR ", msg);
+            },
+        },
+    }
+);
+
+const closeTracer = promisify(tracer.close);
+
+const spans = {};
 
 (async () => {
     const server = hapi.server({
@@ -31,6 +61,50 @@ const marketSummaryController = require("./src/controller/marketsummary");
 
     marketSummaryController(server);
 
+    server.ext("onPreHandler", (request, h) => {
+        // console.log("onPreHandler headers", request.headers);
+
+        console.log("spans", spans);
+
+        const parent = tracer.extract(
+            opentracing.FORMAT_HTTP_HEADERS,
+            request.headers
+        );
+
+        // if (parent.toSpanId()) {
+        const span = tracer.startSpan("findMarketSummary", {
+            // childOf: parent,
+            tags: {
+                "http.locale": request.headers["accept-language"],
+            },
+        });
+
+        tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, request.headers);
+        // }
+
+        spans[span.context().toSpanId()] = span;
+
+        return h.continue;
+    });
+
+    server.ext("onPreResponse", (request, h) => {
+        const spanContext = tracer.extract(
+            opentracing.FORMAT_HTTP_HEADERS,
+            request.headers
+        );
+
+        if (spanContext.toSpanId()) {
+            const span = spans[spanContext.toSpanId()];
+
+            if (span) {
+                span.finish();
+                delete spans[spanContext.toSpanId()];
+            }
+        }
+
+        return h.continue;
+    });
+
     await server.start();
 
     console.log("Server running on %s", server.info.uri);
@@ -53,6 +127,7 @@ const marketSummaryController = require("./src/controller/marketsummary");
 const stop = async (server, code = 0) => {
     try {
         await server.stop();
+        await closeTracer();
         await repository.stop();
     } catch (e) {
         console.log("Error stopping server", e);
--- src/controller/marketsummary.js
+++ src/controller/marketsummary.js
@@ -4,7 +4,11 @@ const marketsummary = (server) => {
     server.route({
         method: "GET",
         path: "/marketsummary",
-        handler: async (request, h) => getMarketSummary(),
+        handler: (request, h) => {
+            // console.log("controller headers", request.headers);
+
+            return getMarketSummary();
+        },
     });
 };
 
