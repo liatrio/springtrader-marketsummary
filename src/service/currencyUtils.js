dedf08f17f533faf9c68c8ee919eb009a888bfbb
Sean Jordan <jordansean10@gmail.com>
2020-06-10 10:52:56 -0700

    Added currencyUtils.js

---
+++ src/service/currencyUtils.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch");
+const LocaleCurrency = require('locale-currency');
+const BigNumber = require('bignumber.js');
+let rates;
+
+const loadRates = async () => {
+    const response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    const currentRates = await response.json();
+    rates = currentRates;
+} 
+
+const getRate = async (currencyCode) => {
+    if (!rates) {
+        await loadRates();
+    } 
+
+    return rates[currencyCode];
+}
+
+const convertCurrency = async (marketSummary, locale) => {
+    const tradeStockIndexAverage = new BigNumber(marketSummary.tradeStockIndexAverage);
+    const tradeStockIndexOpenAverage = new BigNumber(marketSummary.tradeStockIndexOpenAverage);
+    
+    const currencyCode = LocaleCurrency.getCurrency(locale);
+    
+    if (!currencyCode) {
+        throw error;
+    }
+
+    const exchangeRate = await getRate(currencyCode);
+    const bigNumberExchangeRate = new BigNumber(exchangeRate);
+
+    marketSummary.tradeStockIndexAverage = tradeStockIndexAverage.times(exchangeRate).toNumber();
+    marketSummary.tradeStockIndexOpenAverage = tradeStockIndexOpenAverage.times(exchangeRate).toNumber();
+
+
+    return marketSummary;
+}
+
+module.exports = {
+    convertCurrency,
+};
+
