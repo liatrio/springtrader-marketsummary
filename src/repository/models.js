be9638f82a3947aa62a85899ccc18c225c6107ff
Ryan Vance <ryanv@liatrio.com>
2020-09-29 11:12:05 -0500

    Configure one-time setup of increment plugin for initial data load

--- src/repository/models.js
+++ src/repository/models.js
@@ -3,13 +3,20 @@ const autoIncrementFactory = require("mongoose-sequence");
 const repository = require("./index");
 const quoteSchema = require("./schema/quote");
 
+let setup = false;
+
 const Quote = () => {
     const connection = repository.getConnection();
-    const autoIncrement = autoIncrementFactory(connection);
 
-    quoteSchema.plugin(autoIncrement, {
-        inc_field: "quoteid",
-    });
+    if (!setup) {
+        const autoIncrement = autoIncrementFactory(connection);
+
+        quoteSchema.plugin(autoIncrement, {
+            inc_field: "quoteid",
+        });
+
+        setup = true
+    }
 
     return connection.model("Quote", quoteSchema);
 };
